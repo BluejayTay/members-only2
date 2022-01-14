cb20b44c566ae2de14ba2831620202b5e08ca8dd
BluejayTay <tayrusthoven@gmail.com>
2022-01-14 15:54:29 -0600

    Add Post scaffold and user foreign key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :username, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
